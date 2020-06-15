823d45e176e22e41c8ed4058f77c5a624e408768
JYWen <wenzi19892003@126.com>
2020-06-15 23:10:16 +0800

    added lession 4 20-22

--- Lesson4/lession4.py
+++ Lesson4/lession4.py
@@ -292,3 +292,52 @@ for index, name in enumerate(cast):
     cast[index] = name + " " + str(heights[index])
 print(cast)
 print("\n")
+
+
+#Part 21 List Comprehension
+cities = ["los angel", "shang hai", "bei jing", "new york city"]
+capitalized_cities = []
+for city in cities:
+    capitalized_cities.append(city.title())
+
+#same as:
+    
+capitalized_cities = [city.title() for city in cities]
+
+print(capitalized_cities)
+
+squares = [x ** 2 for x in range(9)]
+print(squares)
+
+squares = [x ** 2 for x in range(9) if x % 2 == 0]
+print(squares)
+
+squares = [x ** 2 if x % 2 == 0 else x + 3 for x in range(9)]
+print(squares)
+print("\n")
+
+
+#Part 22 quiz 1
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name[: name.find(" ")].lower() for name in names]
+#first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+
+#Part 22 quiz 2
+multiples_3 = [x * 3 for x in range(1, 21)]
+print(multiples_3)
+
+
+#Part 22 quiz 3
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
